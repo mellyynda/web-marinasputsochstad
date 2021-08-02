1f75bcdb17508cabd3db62bef475673fb193ae40
Melinda <melinda.sava.td@gmail.com>
2021-08-02 18:36:32 +0200

    added footer details

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,24 +2,32 @@
   <footer>
     <div class="half">
       <h4>FÖLJ OSS</h4>
-      <img src="" alt="">
-      <img src="" alt="">
-      <a href=""></a>
+      <img src="../assets/icons/social/facebook.png" alt="gå till facebook">
+      <img src="../assets/icons/social/instagram.png" alt="gå till instagram">
+      <a href="">Så hanterar vi Covid-19</a>
       <small>Marinas Puts &#38; Städ  AB 2021</small>
       <small><a href="https://github.com/mellyynda">Website created by @mellyynda © Alla rättigheter förbehållna</a></small>
     </div>
     <div class="half">
       <h4>KONTAKTA OSS</h4>
+      <TheContactDetails />
     </div>
   </footer>
 </template>
 
 <script>
-export default {
+import TheContactDetails from './TheContactDetails.vue'
 
+export default {
+  components: {
+    TheContactDetails
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+footer {
+  background-color: #27763D;
+  color: #fff;
+}
 </style>
