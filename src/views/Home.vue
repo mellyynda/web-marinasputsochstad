f2180171f3d142cbae6b7440908f07b4e5652be5
Melinda <melinda.sava.td@gmail.com>
2021-08-03 15:45:06 +0200

    almost fixed hompage background image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home">
-    <h1>Home page</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum voluptas nostrum deserunt! Repellendus, quo quisquam! Voluptatibus reprehenderit optio maiores placeat distinctio perspiciatis itaque, tempora quaerat commodi quasi repudiandae vitae enim?</p>
-  </div>
+    <div class="cover">
+      <img src="../assets/home/clean-desk.jpg" alt="ren skrivbord">
+      <h1>PRISVÄRD STÄDNING OCH FÖNSTERPUTS</h1>
+    </div>
 </template>
 
 <script>
@@ -16,3 +16,31 @@ export default {
   }
 }
 </script>
+<style scoped>
+.cover {
+  height: 100%;
+  /* object-fit: cover; */
+  overflow: hidden;
+  position:relative;
+  /* background-image: url("../assets/home/clean-desk-wide.jpg");
+  background-size: cover;
+  background-position:50% 85%;
+  background-repeat: no-repeat; */
+}
+.cover>img{
+  /* max-height: 100%; */
+  width: 150%;
+  position: absolute;
+  bottom: 0;
+  left: -25%;
+  z-index: -1;
+}
+
+@media screen and (min-width:950px) {
+  .cover>img {
+    /* bottom: 55%; */
+     /* background-image: url("../assets/home/clean-desk-mobile.png"); */
+     /* height: 50%; */
+  }
+}
+</style>
