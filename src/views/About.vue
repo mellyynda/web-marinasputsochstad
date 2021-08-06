4a3844c24125cc23c1ddb0cfdf64148658436add
Melinda <melinda.sava.td@gmail.com>
2021-08-06 13:30:03 +0200

    added elements for about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <img src="../assets/about/hands.jpg" alt="holding hands">
+    <h2> <img src="../assets/about/house.png" alt="house"> Den lilla familjeföretag </h2>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus libero numquam aliquam illum quo minus unde in eligendi maiores officia veritatis ipsum sapiente natus, nam harum soluta quisquam aperiam modi.</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi nobis, sed temporibus iusto tempora ut optio ipsum aliquid esse autem, nesciunt quae modi vero laudantium recusandae quos! Quia, quae pariatur?</p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      pic: "../assets/about/hands.jpg"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
